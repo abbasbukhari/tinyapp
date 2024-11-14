1ea8d367ddd312cedf328452315ca396e4f24b98
abbas <abbasbukhari2015@gmail.com>
2024-11-14 18:23:57 -0500

    Add EJS template rendering and root route

--- express_server.js
+++ express_server.js
@@ -2,24 +2,30 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // Default port 8080
 
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+
+// Database of URLs (can be expanded later)
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+// Root route - renders the index.ejs template with dynamic data
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  const templateVars = { title: "Welcome to TinyApp" };
+  res.render("index", templateVars);
 });
 
-// Add the '/hello' route that returns HTML
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+// JSON route for URL database (this can be expanded later)
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Listen on the specified port
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+   
